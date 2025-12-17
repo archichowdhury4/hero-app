b843a0df2c0ae93cdc53a72297cd011425cc7231
archichowdhury4 <archicmpi@gmail.com>
2025-12-17 21:45:50 +0600

    created trusted apps page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Components/Banner';
+import TrustedApps from '../Components/TrustedApps';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <TrustedApps></TrustedApps>
         </div>
     );
 };
